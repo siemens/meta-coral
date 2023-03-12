siemens/meta-coral

commit 1666fc65c8bb9cfa29a1360f0d96579381dcfe5b

chore: split pycoral deps into runtime and build-time
The dependency to python3-tflite-runtime is only
needed at runtime. Hence, also do not build-depend on it.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

recipes-python/pycoral/pycoral_2.0.0.bb
@@ -15,10 +15,8 @@ require recipes-devtools/bazel-toolchains/bazel-toolchains.inc
 
 PR  = "3"
 
-DEPENDS = " \
-    libedgetpu \
-    python3-tflite-runtime \
-"
+DEPENDS += "libedgetpu-dev"
+RDEPENDS += "python3-tflite-runtime"
 
 # apply patches with git to not leave any traces
 PATCHTOOL = "git"

commit d8c49ede72cc9efb70944bd648dd7cd1e304ec2b

pycoral: fix bitbake build dependencies
Those got subtly broken by 1666fc65: Builds of those missing deps were
racing with the slower pycoral, usally winning, thus papering over this
mistake.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-python/pycoral/pycoral_2.0.0.bb
@@ -15,7 +15,14 @@ require recipes-devtools/bazel-toolchains/bazel-toolchains.inc
 
 PR  = "3"
 
-DEPENDS += "libedgetpu-dev"
+DEPENDS += " \
+    libedgetpu-dev \
+    python3-absl \
+    python3-astunparse \
+    python3-gast \
+    python3-keras-preprocessing \
+    python3-opt-einsum \
+    "
 RDEPENDS += "python3-tflite-runtime"
 
 # apply patches with git to not leave any traces